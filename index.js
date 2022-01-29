c748c3329ff3931a9ea4ae384a58cb4b8f1b77b3
xiakele <xiakele123@gmail.com>
2022-01-30 00:43:36 +0800

    [功能] 增加获取制定数量书目的功能，修复海量bug

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express=require("express")
-const app=express()
-const fs=require('fs').promises
-const port=3030
-const douban=require("./scrapers/douban")
+const express = require("express")
+const app = express()
+const fs = require('fs').promises
+const port = 3030
+const douban = require("./scrapers/douban")
 
-async function startScraper(func){
+async function startScraper(func) {
     await func()
-    setInterval(func,60000)
+    setInterval(func, 10800000)
 }
 
 startScraper(douban)
 
-app.listen(port,()=>{
-    console.log(`api.bookmeet.tk running at 127.0.0.1:${port}`)
+app.listen(port, () => {
+    console.log(`api.bookmeet.tk running at http://127.0.0.1:${port}`)
 })
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Welcome to api.bookmeet.tk!")
 })
-app.get("/douban",async (req,res)=>{
-    res.send(String(await fs.readFile("./results/douban")))
+app.get("/douban", async (req, res) => {
+    if (req.query.n) {
+        tmp = JSON.parse(await fs.readFile("./results/douban.json"))
+        tmp.data = tmp.data.slice(end=parseInt(req.query.n))
+        res.json(tmp)
+    } else {
+        res.json(JSON.parse(await fs.readFile("./results/douban.json")))
+    }
 })
--- scrapers/douban.js
+++ scrapers/douban.js
@@ -1,14 +1,25 @@
-const puppeteer=require("puppeteer")
-const fs=require("fs").promises
-async function getBooks(page){
-    return page.$$eval(".chart-dashed-list>li",items=>items.map(item=>`{"name":"${item.querySelector(".clearfix>a").innerHTML}","img":"${item.querySelector(".media__img>a").href}"}`))
+const puppeteer = require("puppeteer")
+const fs = require("fs").promises
+async function getBooks(page) {
+    return page.$$eval(".chart-dashed-list>li", items => items.map(item => {
+        const name = item.querySelector(".clearfix>a").innerHTML
+        const author = item.querySelector(".subject-abstract").innerHTML.trim().split(" / ")[0]
+        const img = item.querySelector(".media__img>a").href
+        const rate = Number(item.querySelector(".font-small").innerHTML)
+        const json = { "name": name, "author": author, "img": img, "rate": rate }
+        return json
+    }))
 }
-module.exports=async function start(){
-    const browser=await puppeteer.launch({defaultViewport:{width:1920,height:1080}})
-    const page=await browser.newPage()
-    await page.goto("https://book.douban.com/latest?tag=%E5%85%A8%E9%83%A8")
-    books=await getBooks(page)
-    await fs.writeFile("./results/douban",`[${books}]`)
+module.exports = async function start() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    let books = []
+    for (let i = 1; i <= 15; i++){
+        await page.goto(`https://book.douban.com/latest?tag=%E5%85%A8%E9%83%A8&p=${i}`)
+        books=books.concat(await getBooks(page))
+    }
+    books = {"data":books}
+    await fs.writeFile("./results/douban.json", JSON.stringify(books))
     console.log(`douban refreshed at ${new Date()}`)
     await browser.close()
 }
