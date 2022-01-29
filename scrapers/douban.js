beb3c270b0e0d2ca0cfc562b1ddddd43d3f73635
xiakele <xiakele123@gmail.com>
2022-01-29 17:30:42 +0800

    [修复] 删除proxy配置

--- scrapers/douban.js
+++ scrapers/douban.js
@@ -4,7 +4,7 @@ async function getBooks(page){
     return page.$$eval(".chart-dashed-list>li",items=>items.map(item=>`{"name":"${item.querySelector(".clearfix>a").innerHTML}","img":"${item.querySelector(".media__img>a").href}"}`))
 }
 exports.start=async function start(){
-    const browser=await puppeteer.launch({defaultViewport:{width:1920,height:1080},args:["--proxy-server=http://127.0.0.1:8889"]})
+    const browser=await puppeteer.launch({defaultViewport:{width:1920,height:1080}})
     const page=await browser.newPage()
     await page.goto("https://book.douban.com/latest?tag=%E5%85%A8%E9%83%A8")
     books=await getBooks(page)
