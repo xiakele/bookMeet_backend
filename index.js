6e85c5d913456e68e6134617a0595fc3cff24ed0
xiakele <xiakele123@gmail.com>
2022-01-29 14:55:13 +0800

    [功能] 使用puppeteer重构代码

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const app=express()
+const fs=require("fs/promises")
+const port=3030
+const douban=require("./scrapers/douban")
+
+setInterval(douban.start,60000)
+
+app.listen(port,()=>{
+    console.log(`api.bookmeet.tk running at 127.0.0.1:${port}`)
+})
+app.get("/",(req,res)=>{
+    res.send("Welcome to api.bookmeet.tk!")
+})
+app.get("/douban",async (req,res)=>{
+    res.send(String(await fs.readFile("./results/douban")))
+})
