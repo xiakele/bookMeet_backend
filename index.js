525f7d8045a11a00420823b5945724f8f25db13d
xiakele <xiakele123@gmail.com>
2022-01-30 00:51:14 +0800

    [修复] 数量参数传递失效

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function startScraper(func) {
     setInterval(func, 10800000)
 }
 
-startScraper(douban)
+// startScraper(douban)
 
 app.listen(port, () => {
     console.log(`api.bookmeet.tk running at http://127.0.0.1:${port}`)
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.get("/douban", async (req, res) => {
     if (req.query.n) {
         tmp = JSON.parse(await fs.readFile("./results/douban.json"))
-        tmp.data = tmp.data.slice(end=parseInt(req.query.n))
+        tmp.data = tmp.data.slice(0,parseInt(req.query.n))
         res.json(tmp)
     } else {
         res.json(JSON.parse(await fs.readFile("./results/douban.json")))
