79a8eaf00c38cca47c7f90a62fc2941ee47c7433
xiakele <xiakele123@gmail.com>
2022-07-08 23:00:27 +0800

    [修复] 更换豆瓣搜索地址

--- scrapers/search.js
+++ scrapers/search.js
@@ -1,13 +1,13 @@
 async function getBooks(page) {
-    if (! await page.$(".result-list > .result")) {
+    if (! await page.$(".sc-bZQynM")) {
         return []
     }
-    return page.$$eval(".result-list > .result", items => items.map(item => {
-        const name = item.querySelector(".nbg").title
-        const author = item.querySelector(".subject-cast") ? item.querySelector(".subject-cast").innerHTML.split(" / ")[0] : ""
+    return page.$$eval(".sc-bZQynM", items => items.map(item => {
+        const name = item.querySelector(".title-text").innerHTML
+        const author = item.querySelector(".abstract") ? item.querySelector(".abstract").innerHTML.split(" / ")[0] : ""
         const img = item.querySelector("img").src
         const rate = item.querySelector(".rating_nums") ? Number.parseFloat(item.querySelector(".rating_nums").innerHTML) : 0
-        const url = item.querySelector(".nbg").href
+        const url = item.querySelector(".title-text").href
         const json = { "name": name, "author": author, "img": img, "rate": rate, "url": url }
         return json
     }))
@@ -15,7 +15,8 @@ async function getBooks(page) {
 
 module.exports = async function start({ page, data: { query } }) {
     try {
-        const statCode = (await page.goto(`https://www.douban.com/search?cat=1001&q=${query}`, { waitUntil: "domcontentloaded" })).status()
+        await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0")
+        const statCode = (await page.goto(`https://search.douban.com/book/subject_search?search_text=${query}`, { waitUntil: "domcontentloaded" })).status()
         if (statCode == 403) {
             throw new Error("access to douban is restricted")
         }
